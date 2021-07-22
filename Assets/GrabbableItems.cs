df452d8d45d724f0657754f811fa5e82e0fc3b23
Kshitij Gajapure <kshitijiitgn@gmail.com>
2021-07-22 16:16:04 +0530

    Added basic Quest interactions like controller movement and grab functionality.

---
+++ Assets/GrabbableItems.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabbableItems : OVRGrabbable
+{
+
+    [SerializeField] private Material isGrabbedMaterial;
+    private Material originalMaterial;
+
+    private Renderer _renderer;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        _renderer = GetComponent<Renderer>();
+        originalMaterial = _renderer.material;
+    }
+
+    public override void GrabBegin(OVRGrabber hand, Collider grabPoint)
+    {
+        base.GrabBegin(hand, grabPoint);
+        _renderer.material = isGrabbedMaterial;
+    }
+
+
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        base.GrabEnd(linearVelocity, angularVelocity);
+        _renderer.material = originalMaterial;
+    }
+
+
+}
