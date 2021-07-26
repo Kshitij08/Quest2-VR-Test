9dcdadcbc9c941ea01fd9fbc6a33463d307dde3b
Kshitij Gajapure <kshitijiitgn@gmail.com>
2021-07-26 16:24:12 +0530

    Added Triggers to handle in-game interactions in the "Button Trigger" scene

---
+++ Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ButtonTrigger : MonoBehaviour
+{
+
+    public static event Action onButtonPressed;
+
+    public Material isInteractingMaterial;
+    public Material defaultMaterial;
+
+    //private Animator buttonAnimator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultMaterial = GetComponent<Renderer>().material;
+        //buttonAnimator = GetComponent<Animator>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            GetComponent<Renderer>().material = isInteractingMaterial;
+            //buttonAnimator.SetTrigger("ButtonPressed");
+            onButtonPressed?.Invoke();
+        }
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<Renderer>().material = isInteractingMaterial;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<Renderer>().material = defaultMaterial;
+        }
+    }
+
+}
---
+++ Assets/Scripts/ShowInteractables.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowInteractables : MonoBehaviour
+{
+
+    public GameObject interactables;
+
+    private void Start()
+    {
+        interactables.SetActive(false);
+    }
+
+
+    private void OnEnable()
+    {
+        ButtonTrigger.onButtonPressed += ActivateInteractables;
+    }
+
+    private void OnDisable()
+    {
+        ButtonTrigger.onButtonPressed -= ActivateInteractables;
+    }
+
+    private void ActivateInteractables()
+    {
+        interactables.SetActive(true);
+    }
+
+
+}
