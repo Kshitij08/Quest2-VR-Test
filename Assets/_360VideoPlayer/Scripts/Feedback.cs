027f1154f4d1de1528f8b983549be5976af8add7
Kshitij Gajapure <kshitijiitgn@gmail.com>
2021-07-27 15:40:27 +0530

    Added 360 Video Player with ability to play, pause, and skip the video(s)

---
+++ Assets/_360VideoPlayer/Scripts/Feedback.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class Feedback : MonoBehaviour
+{
+
+    public float distance = 2.5f;
+
+    public VideoManager videoManager = null;
+
+    [Header("Icons")]
+    public Sprite pause = null;
+    public Sprite load = null;
+
+
+    private SpriteRenderer spriteRenderer = null;
+
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        SetupWithCamera();
+
+        videoManager.onPause.AddListener(TogglePause);
+        videoManager.onPause.AddListener(ToggleLoad);
+
+
+    }
+
+    private void OnDestroy()
+    {
+        videoManager.onPause.RemoveListener(TogglePause);
+        videoManager.onPause.RemoveListener(ToggleLoad);
+
+
+
+    }
+
+    private void SetupWithCamera()
+    {
+        transform.parent = Camera.main.transform;
+        transform.localRotation = Quaternion.identity;
+        transform.localPosition = new Vector3(0, 0, distance);
+    }
+
+    private void TogglePause(bool isPaused)
+    {
+        spriteRenderer.sprite = pause;
+        spriteRenderer.enabled = isPaused;
+    }
+
+    private void ToggleLoad(bool isLoaded)
+    {
+        spriteRenderer.sprite = load;
+        spriteRenderer.enabled = isLoaded;
+    }
+
+}
---
+++ Assets/_360VideoPlayer/Scripts/InputManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    public VideoManager videoManager = null;
+
+    private void Update()
+    {
+
+        if (!videoManager.IsVideoReady)
+        {
+            return;
+        }
+
+
+        OculusInput();
+        KeyboardInput();
+    }
+
+    private void OculusInput()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.All))
+        {
+            videoManager.PauseToggle();
+        }
+
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.LTouch))
+        {
+            videoManager.PreviousVideo();
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch))
+        {
+            videoManager.NextVideo();
+
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
+        {
+            videoManager.SeekBack();
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+        {
+            videoManager.SeekForward();
+
+        }
+    }
+
+    private void KeyboardInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            videoManager.PauseToggle();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            videoManager.PreviousVideo();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            videoManager.NextVideo();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            videoManager.SeekBack();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            videoManager.SeekForward();
+
+        }
+    }
+}
